0e95c470ff0e83ab9ab45344f631efa263b44f9f
maxkulbako <mkulbako@gmail.com>
2025-02-26 15:05:59 +0100

    feat: add base card layout

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -7,6 +7,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import ProductPrice from "./product-price";
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
@@ -30,7 +31,7 @@ const ProductCard = ({ product }: { product: any }) => {
         <div className="flex-between gap-4">
           <p>{product.rating} Stars</p>
           {product.stock > 0 ? (
-            <p className="text-bold">{product.price}</p>
+            <ProductPrice value={Number(product.price)} />
           ) : (
             <p className="text-destructive">Out of stock</p>
           )}
