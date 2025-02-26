9868fe03c3954b86d8a13aee3ef77f72daa80ffb
maxkulbako <mkulbako@gmail.com>
2025-02-26 13:53:06 +0100

    feat: add side menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
 
 const Header = () => {
   return (
@@ -22,17 +20,7 @@ const Header = () => {
           </span>
         </Link>
         <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart className="h-4 w-4" /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/login">
-              <UserIcon className="h-4 w-4" /> Sign in
-            </Link>
-          </Button>
+          <Menu />
         </div>
       </div>
     </header>
