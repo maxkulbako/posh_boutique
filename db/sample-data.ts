7273db5bcad6c6e17f1a5794f65a0ac691c78b34
maxkulbako <mkulbako@gmail.com>
2025-02-27 18:34:26 +0100

    feat: setup base NextAuth configuration

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,4 +1,20 @@
+import { hashSync } from 'bcrypt-ts-edge';
+
 const sampleData = {
+  users: [
+    {
+      name: 'Max',
+      email: 'admin@example.com',
+      password: hashSync('adminpass', 10),
+      role: 'admin',
+    },
+    {
+      name: 'Jane',
+      email: 'jane@example.com',
+      password: hashSync('userpass', 10),
+      role: 'user',
+    },
+  ],
   products: [
     {
       name: 'Polo Sporting Stretch Shirt',
