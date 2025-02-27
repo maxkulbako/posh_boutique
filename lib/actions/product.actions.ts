a9cac3f2abff44b789a53f9e7c8683bdb9b0b859
maxkulbako <mkulbako@gmail.com>
2025-02-27 13:15:03 +0100

    feat: add base product detail page layout

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -11,3 +11,11 @@ export async function getLatestProducts() {
     });
     return convertToPlainObject(data);
 }
+
+// Get single product by slug
+export async function getProductBySlug(slug: string) {
+    const data = await prisma.product.findFirst({
+        where: { slug: slug },
+    });
+    return convertToPlainObject(data);
+}
