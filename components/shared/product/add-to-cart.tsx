3a6b9fd155fb5aab43a0084a49998a0f230bb975
maxkulbako <mkulbako@gmail.com>
2025-03-03 17:03:31 +0100

    feat: add toast messages

---
+++ components/shared/product/add-to-cart.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+import { CartItem } from '@/types';
+import { Plus } from 'lucide-react';
+import { toast } from 'sonner';
+import { addItemToCart } from '@/lib/actions/cart.actions';
+
+const AddToCart = ({ item }: { item: CartItem }) => {
+  const router = useRouter();
+
+  const handleAddToCart = async () => {
+    const res = await addItemToCart(item);
+
+    if (!res.success) {
+      toast.error('There was an issue');
+      return;
+    }
+
+    toast.success(`${item.name} added to cart`, {
+      action: {
+        label: 'Go To Cart',
+        onClick: () => router.push('./cart'),
+      },
+    });
+  };
+
+  return (
+    <Button className="w-full" type="button" onClick={handleAddToCart}>
+      <Plus />
+      AddToCart
+    </Button>
+  );
+};
+
+export default AddToCart;
