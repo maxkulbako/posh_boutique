e5d1f25766c4c7fa6bda4f18d52ea917d4b36b85
maxkulbako <mkulbako@gmail.com>
2025-03-05 19:08:53 +0100

    feat: add subtotal cart to cart page

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -16,6 +16,8 @@ import {
   TableCell,
 } from '@/components/ui/table';
 import { Button } from '@/components/ui/button';
+import { formatCurrency } from '@/lib/utils';
+import { Card, CardContent } from '@/components/ui/card';
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
   const router = useRouter();
@@ -114,6 +116,29 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
               </TableBody>
             </Table>
           </div>
+          <Card>
+            <CardContent className="p-4 gap-4">
+              <div className="pb-3 text-xl">
+                Subtotal ({cart.items.reduce((acc, item) => acc + item.qty, 0)}
+                ):
+                <span className="font-bold">
+                  {' '}
+                  {formatCurrency(cart.itemsPrice)}
+                </span>
+              </div>
+              <Button
+                className="w-full"
+                disabled={isPending}
+                onClick={() => {
+                  startTransition(() => {
+                    router.push('/shipping-address');
+                  });
+                }}
+              >
+                <ArrowRight className="w-4 h-4" /> Proceed to Checkout
+              </Button>
+            </CardContent>
+          </Card>
         </div>
       )}
     </>
